620aad6bc9378ca5878526b46bd72049f9917d10
Виктория <vvpoletaeva@bk.ru>
2022-04-24 23:55:35 +0300

    task_01

--- 03_module/src/task_01/Main.java
+++ 03_module/src/task_01/Main.java
@@ -6,18 +6,23 @@ public class Main {
     public static void main(String[] args) {
         int firstValue = 1;
         int lastValue = 12;
-        int result = 1;
+        int factorial = 1;
 
-        System.out.println("Введите число");
+
+        System.out.println("Р’РІРµРґРёС‚Рµ С‡РёСЃР»Рѕ");
 
 
         int value = new Scanner(System.in).nextInt();
-        for (value = firstValue; value <= lastValue; value = value + 1) {
-            result = result * value;
 
-        System.out.println("Факториал равен:" + result);
+        for (value = 1; value <= 12; value = value - 1) {
+            factorial  *= value;
+
+        }
+        System.out.println("Р¤Р°РєС‚РѕСЂРёР°Р» СЂР°РІРµРЅ:" + factorial);
+
         }
 
+    }
 
-        System.out.println("Факториал не найден");
-}
+
+
