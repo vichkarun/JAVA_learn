0ccc903bbeccee5114f1aa413b2ceb50853a434c
user1836 <vvpoletaeva@bk.ru>
2022-04-25 14:14:46 +0300

    task_02 done

--- 03_module/src/task_02/Main.java
+++ 03_module/src/task_02/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Введите число");
+
         int value = new Scanner(System.in).nextInt();
 
         for (int i = 1; i <= value; i = i + 1) {
