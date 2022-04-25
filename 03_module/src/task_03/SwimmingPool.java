ed29da88776bb5c7792deb159736fe0cdb33951d
user1836 <vvpoletaeva@bk.ru>
2022-04-25 17:52:30 +0300

    task_03

--- 03_module/src/task_03/SwimmingPool.java
+++ 03_module/src/task_03/SwimmingPool.java
@@ -1,18 +1,18 @@
 package task_03;
 
-public class Main {
+public class SwimmingPool {
     public static void main(String[] args) {
 
         int volume = 1200;
 
         int fillingSpeed = 30; //30 litres per minute
         int devastationSpeed = 10; //10 litres per minute
-        int currentVolume = 1;
+        int currentVolume = 0;
         int fillingTime = 1;
 
         while (currentVolume != volume) {
-            if (currentVolume == (volume + fillingSpeed - devastationSpeed));
-            System.out.println(" оличество литров воды в бассейне в 1 итерации = " + currentVolume);
+            if (currentVolume == currentVolume + fillingSpeed - devastationSpeed);
+            System.out.println(" = " + currentVolume);
         }
 
 
