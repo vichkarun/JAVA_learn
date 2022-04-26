0482d635e6cb3b2d0c91487706066cc5883d4ea4
user1836 <vvpoletaeva@bk.ru>
2022-04-26 10:39:36 +0300

    task_02 done

--- 03_module/src/task_02/ForCycle.java
+++ 03_module/src/task_02/ForCycle.java
@@ -2,7 +2,7 @@ package task_02;
 
 import java.util.Scanner;
 
-public class Main {
+public class ForCycle {
     public static void main(String[] args) {
 
         System.out.println("Введите число");
